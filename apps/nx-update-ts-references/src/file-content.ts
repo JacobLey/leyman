233f0b6ba6c77c3b9a10ec3c67de9659cb96e475
Jacob Ley <jacobtley@gmail.com>
2024-08-09 04:00:51 +0000

    nx-update-ts-references: Fix variable name

--- apps/nx-update-ts-references/src/file-content.ts
+++ apps/nx-update-ts-references/src/file-content.ts
@@ -1,9 +1,9 @@
 import type { PopulateFileParams } from 'npm-load-populate-files';
-import populateFilesOptionsSchema from './executors/update-ts-references/schema.js';
+import updateTsReferencesOptionsSchema from './executors/update-ts-references/schema.js';
 
 export default [
     {
         filePath: './src/executors/update-ts-references/schema.json',
-        content: populateFilesOptionsSchema,
+        content: updateTsReferencesOptionsSchema,
     },
 ] satisfies PopulateFileParams[];
