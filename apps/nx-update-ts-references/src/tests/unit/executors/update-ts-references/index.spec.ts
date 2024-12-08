fe6c03a5d4e6d974aa9ca89503278fa5eb0527ff
Jacob Ley <jacobtley@gmail.com>
2024-12-08 06:30:44 +0000

    nx-update-ts-references: Create plugin integration test

--- apps/nx-update-ts-references/src/tests/unit/executors/update-ts-references/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { suite, test } from 'mocha-chain';
-
-suite('types', () => {
-    test('coverage', async () => {
-        await import('../../../../executors/update-ts-references/index.cjs');
-    });
-});
