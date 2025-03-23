e9cb3f5f1b297abef3d5ce94349995fcb9585984
Jacob Ley <jacobtley@gmail.com>
2025-03-23 03:27:12 +0000

    Bump dagger to 0.17.0

--- scripts/dagger-develop.sh
+++ scripts/dagger-develop.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+find ./dagger -name go.mod \
+    -exec sh -c ' \
+        head -n 1 "$1" | \
+        grep -q "^module dagger/" && \
+        sed -i "/^go 1\.23\.0$/q" "$1" \
+    ' _ {} \;
+
+find ./dagger -name go.sum \
+    -exec sh -c '> "$1"' _ {} \;
+
 # Helper script to "recursively" setup all dagger modules for local development
 dagger develop --mod ./dagger/modules/debian
 dagger develop --mod ./dagger/modules/go-lang
