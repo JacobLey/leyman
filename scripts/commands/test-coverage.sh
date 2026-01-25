d28e89d8d053d0bb4f2ca19c7d7d63154b667da1
Jacob Ley <jacobtley@gmail.com>
2026-01-25 23:46:49 +0000

    Compute coverage across all projects
    Now just by using a method in another project counts as coverage

---
+++ scripts/commands/test-coverage.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+function test_coverage() {
+    nx run-many -t test
+    rm -rf ./.coverage/tmp
+    mkdir -p ./.coverage/tmp
+    cp -r ./.coverage/project/*/tmp/* ./.coverage/project/@*/*/tmp/* ./.coverage/tmp
+    nx run-many -t coverage-report
+}
