8cfddd628007d945ed549eed99679b57ea64c170
Jacob Ley <jacobtley@gmail.com>
2026-01-27 00:16:15 +0000

    Fix caching for linter
    Issue was wrong cache output, but really don't need to provide any of these options so long as a new executor isn't provided
    
    Also two commands that run in parallel-ish both caching to the same output risks overlap. Separated out into different directories that end up getting merged

--- scripts/commands/test-coverage.sh
+++ scripts/commands/test-coverage.sh
@@ -4,6 +4,6 @@ function test_coverage() {
     nx run-many -t test
     rm -rf ./.coverage/tmp
     mkdir -p ./.coverage/tmp
-    cp -r ./.coverage/project/*/tmp/* ./.coverage/project/@*/*/tmp/* ./.coverage/tmp
+    cp -r ./.coverage/project/*/tmp/*/* ./.coverage/project/@*/*/tmp/*/* ./.coverage/tmp
     nx run-many -t coverage-report
 }
