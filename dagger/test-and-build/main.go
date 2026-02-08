e8c335655ecd0854363cbea20190dd6b16636344
Jacob Ley <jacobtley@gmail.com>
2026-02-08 05:36:46 +0000

    Less gitignore hardcoding, more explicit

--- dagger/test-and-build/main.go
+++ dagger/test-and-build/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"dagger/test-and-build/internal/dagger"
+	"strings"
 )
 
 type TestAndBuild struct {
@@ -15,42 +16,48 @@ var nodeVersion = "24.13.0"
 var pnpmVersion = "10.28.1"
 
 func New(
+	ctx context.Context,
 	// Root of source file
-	// Ignore needs to mirror .gitignore, in addition to files that are known to not be relevant to build.
-	// Can be copied from Monorepo (auto-populated)
-	// +ignore=[".git","**/*.log*","**/.DS_Store","**/node_modules",".pnpm-store","**/.eslintcache","**/coverage","**/dist","**/.pnpm-lock-hash","**/.swcrc",".nx",".changeset",".devcontainer",".github",".vscode","dagger","!dagger/monorepo/main.go","!dagger/monorepo/monorepo-builder/main.go","go.work","go.work.sum","README.md","scripts"]
+	// Manually ignore the worst offenders, but use actual .gitignore parsing to catch the rest
+	// +ignore=[".git",".claude","**/*.log*","**/.DS_Store","**/node_modules",".pnpm-store","**/.eslintcache",".coverage","**/dist",".nx"]
+	// +defaultPath="../.."
 	source *dagger.Directory,
-) *TestAndBuild {
+) (*TestAndBuild, error) {
+
+	contents, err := source.File(".gitignore").Contents(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	exclude := []string{}
+	for _, line := range strings.Split(contents, "\n") {
+		if line != "" {
+			exclude = append(exclude, line)
+		}
+	}
+
+	// Version controlled, but not relevant to dagger
+	exclude = append(exclude,
+		".git",
+		".gitignore",
+		".changeset",
+		".devcontainer",
+		".github",
+		".vscode",
+		"dagger",
+		"!dagger/monorepo/main.go",
+		"!dagger/monorepo/monorepo-builder/main.go",
+		"go.work",
+		"go.work.sum",
+		"README.md",
+		"scripts",
+	)
+
 	return &TestAndBuild{
 		Source: source.Filter(dagger.DirectoryFilterOpts{
-			Exclude: []string{
-				".git",
-				"**/*.log*",
-				"**/.DS_Store",
-				"**/node_modules",
-				".pnpm-store",
-				"**/.eslintcache",
-				"**/coverage",
-				"**/dist",
-				"**/.pnpm-lock-hash",
-				"**/.swcrc",
-				".nx",
-				// Version controlled, but not relevant to dagger
-				".changeset",
-				".devcontainer",
-				".github",
-				".vscode",
-				"dagger",
-				"!dagger/monorepo/main.go",
-				"!dagger/monorepo/monorepo-builder/main.go",
-				"go.work",
-				"go.work.sum",
-				"README.md",
-				"scripts",
-			},
-		},
-		),
-	}
+			Exclude: exclude,
+		}),
+	}, nil
 }
 
 // CI entrypoint
