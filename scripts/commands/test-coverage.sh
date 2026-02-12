ae8d906d6b4ec3ac29c17a78cb6da16c53f7135e
Jacob Ley <jacobtley@gmail.com>
2026-02-12 05:31:54 +0000

    Fix issue of terminal exit on failed nx

--- scripts/commands/test-coverage.sh
+++ scripts/commands/test-coverage.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-function test_coverage() {
+test_coverage() (
     set -e
     nx run-many -t test --tuiAutoExit --nxBail
     rm -rf ./.coverage/tmp
     mkdir -p ./.coverage/tmp
     cp -r ./.coverage/project/*/tmp/*/* ./.coverage/project/@*/*/tmp/*/* ./.coverage/tmp
     nx run-many -t coverage-report --tuiAutoExit --nxBail
-}
+)
