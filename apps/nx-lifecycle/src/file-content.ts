359f59b5f77f9f697c9092ae0bc19903bccb904b
Jacob Ley <jacobtley@gmail.com>
2026-02-13 05:31:19 +0000

    Transition lifecycle away from nx plugin
    Required installation at root, which isn't desirable

--- apps/nx-lifecycle/src/file-content.ts
+++
@@ -1,9 +0,0 @@
-import type { PopulateFileParams } from 'load-populate-files';
-import lifecycleSchema from './lifecycle/schema.js';
-
-export default [
-    {
-        filePath: './src/lifecycle/schema.json',
-        content: lifecycleSchema,
-    },
-] satisfies PopulateFileParams[];
