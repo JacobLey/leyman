10408df863c17ac3c16ea1376afbf291d87605cc
Jacob Ley <jacobtley@gmail.com>
2026-02-23 04:57:00 +0000

    Remove nx-dagger package

--- apps/nx-dagger/src/tests/unit/executors/generate/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { suite, test } from 'mocha-chain';
-
-suite('entrypoint', () => {
-    test('coverage', async () => {
-        await import('../../../../executors/generate/index.cjs');
-    });
-});
--- apps/nx-dagger/src/tests/unit/generate/lib/types.spec.ts
+++
@@ -1,7 +0,0 @@
-import { suite, test } from 'mocha-chain';
-
-suite('types', () => {
-    test('coverage', async () => {
-        await import('../../../../generate/lib/types.js');
-    });
-});
